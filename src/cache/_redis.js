26eae73e2b89a731a2773e6d00b5ab7df9a2a89a
[15980904537] <[2565051131@qq.com]>
2022-01-14 14:25:09 +0800

    feat：redis操作

---
+++ src/cache/_redis.js
@@ -0,0 +1,59 @@
+/**
+ * @description 连接redis的方法get set
+ * @author vagabond
+ */
+
+const redis = require('redis')
+const { REDIS_CONF } = require('../conf/db')
+
+
+//创建客户端
+const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host);
+
+redisClient.on('error', err => { 
+    console.error('redis error',err)
+})
+
+/**
+ * redis set
+ * @param {string} key 
+ * @param {string} val 
+ * @param {number} timout  过期时间
+ */
+function set(key,val,timout=60*60) { 
+    if (typeof val === 'object') { 
+        val = JSON.stringify(val);
+    }
+    redisClient.set(key, val);
+    redisClient.expire(key,timout)
+}
+/**
+ * redis get
+ * @param {string} key 
+ */
+function get(key) { 
+    const promise = new Promise((resolve, reject) => {
+        redisClient.get(key, (err, val) => {
+            if (err) { 
+                reject(err);
+                return 
+            }
+            if (val === null) {
+                resolve(null);
+                return
+            }
+            try {
+                resolve(JSON.parse(val))
+            } catch { 
+                resolve(val)
+             }
+        })
+        
+    })
+    return promise
+}
+
+module.exports = {
+    set,
+    get
+}
---
+++ src/conf/db.js
@@ -0,0 +1,20 @@
+/**
+ * @description 存储配置
+ * @author vagabond
+ */
+
+const { isProd}=require('../utils/env')
+let REDIS_CONF = {
+    port: 6379,
+    host:'127.0.0.1'
+}
+
+if (isProd) { 
+    REDIS_CONF = {
+        port: 6379,
+        host: '127.0.0.1'
+    }
+}
+module.exports = {
+    REDIS_CONF
+}
