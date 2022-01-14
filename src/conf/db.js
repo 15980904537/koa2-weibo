86d2061499b73dabb50e1f6a685cd1d48cbe5b41
[15980904537] <[2565051131@qq.com]>
2022-01-14 18:38:53 +0800

    feat:mysql 搭建环境

--- src/conf/db.js
+++ src/conf/db.js
@@ -8,13 +8,30 @@ let REDIS_CONF = {
     port: 6379,
     host:'127.0.0.1'
 }
-
+let MYSQL_CONF = {
+    host: 'location',
+    
+    user: 'root',
+    port: 3306,
+    password: 'zhm123456',
+    database:'koa2_weibo_db'
+}
 if (isProd) { 
+    //线上的redis配置
     REDIS_CONF = {
         port: 6379,
         host: '127.0.0.1'
     }
+    //线上的mysql配置
+    MYSQL_CONF = {
+        host: 'location',
+        user: 'root',
+        port: 3306,
+        password: 'zhm123456',
+        database: 'koa2_weibo_db'
+    }
 }
 module.exports = {
-    REDIS_CONF
+    REDIS_CONF,
+    MYSQL_CONF
 }
